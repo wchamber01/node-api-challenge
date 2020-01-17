7708ec85520b77ece5a6b6ca781eeb54b04a0d39
wchamber01 <williamchambers@gmail.com>
2020-01-17 12:15:00 -0500

    added nodemon package

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ Go code!
 */
 const server = require("./server");
 
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 6001;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
